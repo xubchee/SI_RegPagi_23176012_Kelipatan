ab19fb978bc3e8a3391e92490c34d6fae5e4b9b2
xubchee <fajarsubechi.2005@gmail.com>
2024-11-04 22:16:32 +0700

    Add files via upload

---
+++ SI_RegPagi_23176012_Kelipatan.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihan.pertemuan6;
+
+/**
+ *  Nama      : MOHAMMAD FAJAR SUBECHI
+ *  NIM       : 23176012
+ *  Prodi     : Sistem Infprmasi
+ *  Deskripsi : Program ini berisi tentang menghitung tunjangan karyawan yang sudah menikah/belum dan total gaji.
+ * @author ACER
+ */
+public class SI_RegPagi_23176012_Kelipatan {
+public static void main(String[] args) {
+      for(double kelipatan = 3.5; kelipatan <= 35; kelipatan+= 3.5){
+        System.out.println(kelipatan);
+    }
+    }    
+}
